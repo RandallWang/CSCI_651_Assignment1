991f2f1c05a6b7d0812265561c8646846f4c44ca
Randall Wang <randallwang93@gmail.com>
2022-06-03 17:39:57 -0700

    Finish divideAndConquerApproach for LCP

--- LongestCommonPrefix.js
+++ LongestCommonPrefix.js
@@ -21,6 +21,34 @@ export class LongestCommonPrefix {
     }
 
     static divideAndConquerApproach(strings) {
-        return "divideAndConquerApproach";
+        if (strings.length == 2) {
+            return LongestCommonPrefixForTwoStrings(strings[0], strings[1]);
+        }
+        if (strings.length == 1) {
+            return strings[0];
+        }
+        if (strings.length == 0) {
+            return "";
+        }
+
+        let midIndex = Math.floor(strings.length / 2);
+        let firstPart = this.divideAndConquerApproach(strings.slice(0, midIndex));
+        let secondPart = this.divideAndConquerApproach(strings.slice(midIndex));
+        let result = LongestCommonPrefixForTwoStrings(firstPart, secondPart);
+
+        function LongestCommonPrefixForTwoStrings(str1, str2) {
+            let result = "";
+            for (let i = 0; i < str1.length; i++) {
+                let firstSubString = str1.substring(0, i + 1);
+                let secondSubString = str2.substring(0, i + 1);
+                if (firstSubString == secondSubString) {
+                    result = firstSubString;
+                } else {
+                    break;
+                }
+            }
+            return result
+        }
+        return result;
     }
 }
--- main.js
+++ main.js
@@ -5,3 +5,4 @@ import {LongestCommonSubsequence} from "./LongestCommonSubsequence.js";
 //Or use 'npm test' in the terminal to run all the test cases.
 
 // LongestCommonPrefix.bruteForceApproach(["bat","ball","banana","basket","bag","balloon","bad","bald","bar","band","bend","bash","bake","baby","back","brain","basic","bath","basis","balm"]);
+// LongestCommonPrefix.divideAndConquerApproach(["bat","ball","banana","basket","bag","balloon","bad","bald","bar","band","bend","bash","bake","baby","back","brain","basic","bath","basis","balm"]);
