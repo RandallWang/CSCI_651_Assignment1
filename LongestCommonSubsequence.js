846b1a58f1ffdfe8c25376d4530a684614bf5eff
kevinlalala <haibin.xng@gmail.com>
2022-06-03 19:05:11 -0700

    create test.txt

--- LongestCommonSubsequence.js
+++ LongestCommonSubsequence.js
@@ -1,5 +1,18 @@
 export class LongestCommonSubsequence {
     static  bruteForceApproach(str1, str2) {
-        return "bruteForceApproach";
+        let str3 ='';
+        let i = 0;
+        for(let j = 0;j<str2.length;j++){
+       
+            let position = str1.indexOf(str2.charAt(j),i);
+            if (position==-1){
+                continue;
+            }
+            else{
+                i = position;
+                str3 += str1.charAt(i);
+            }
+        }
+        return str3;
     }
 }
